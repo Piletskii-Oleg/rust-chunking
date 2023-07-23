b08d16904b3b06d065ece76c76ab36e31e736344
Piletskii-Oleg <piletskii.oleg@gmail.com>
2023-07-23 21:43:27 +1100

    add secondary condition and bad benchmark

--- src/leap_based.rs
+++ src/leap_based.rs
@@ -1,16 +1,20 @@
 #![allow(dead_code)]
 
+use std::ops::Add;
+use std::time::{Duration, Instant};
+use rand::prelude::ThreadRng;
 use rand::thread_rng;
-use rand_distr::Distribution;
+use rand_distr::{Distribution, Normal};
 
 const MIN_CHUNK_SIZE: usize = 4096; // 4 KB
 const MAX_CHUNK_SIZE: usize = 12288; // 12 KB
 
-const WINDOW_SIZE: usize = 128;
-const WINDOW_COUNT: usize = 24;
-const WINDOW_EXTRA_COUNT: usize = 2;
-const WINDOW_MATRIX_SHIFT: usize = 26; // WINDOW_MATRIX_SHIFT * 4 < WINDOW_SIZE - 5
+const WINDOW_PRIMARY_COUNT: usize = 22;
+const WINDOW_SECONDARY_COUNT: usize = 2;
+const WINDOW_COUNT: usize = WINDOW_PRIMARY_COUNT + WINDOW_SECONDARY_COUNT;
 
+const WINDOW_SIZE: usize = 128;
+const WINDOW_MATRIX_SHIFT: usize = 24; // WINDOW_MATRIX_SHIFT * 4 < WINDOW_SIZE - 5
 const MATRIX_WIDTH: usize = 8;
 const MATRIX_HEIGHT: usize = 255;
 
@@ -19,9 +23,10 @@ enum PointStatus {
     Unsatisfied(usize),
 }
 
-struct Chunk {
-    pos: usize,
-    len: usize,
+#[derive(Debug)]
+pub struct Chunk {
+    pub(super) pos: usize,
+    pub(super) len: usize,
 }
 
 impl Chunk {
@@ -46,13 +51,31 @@ impl Chunker {
     fn generate_matrix() -> Vec<Vec<f64>> {
         let normal = rand_distr::Normal::new(0.0, 1.0).unwrap();
         let mut rng = thread_rng();
-        vec![vec![normal.sample(&mut rng); MATRIX_WIDTH]; MATRIX_HEIGHT]
+
+        (0..MATRIX_HEIGHT)
+            .map(|_| Chunker::generate_row(&normal, &mut rng))
+            .collect()
+    }
+
+    fn generate_row(normal: &Normal<f64>, rng: &mut ThreadRng) -> Vec<f64> {
+        (0..MATRIX_WIDTH)
+            .map(|_| normal.sample(rng))
+            .collect()
     }
 
     fn is_point_satisfied(&self, index: usize, data: &[u8]) -> PointStatus {
-        for i in 0..WINDOW_COUNT {
-            if !self.is_window_qualified(&data[index - i - WINDOW_SIZE..=index - i]) {
+        // primary check, T+1<=x<M where T is WINDOW_SECONDARY_COUNT and M is WINDOW_COUNT
+        for i in WINDOW_SECONDARY_COUNT..WINDOW_COUNT {
+            if !self.is_window_qualified(&data[index - i - WINDOW_SIZE..index - i]) { // window is WINDOW_SIZE bytes long and moves to the left
                 let leap = WINDOW_COUNT - i;
+                return PointStatus::Unsatisfied(leap);
+            }
+        }
+
+        //secondary check, 0<=x<T bytes
+        for i in 0..WINDOW_SECONDARY_COUNT {
+            if !self.is_window_qualified(&data[index - i - WINDOW_SIZE..index - i]) {
+                let leap = WINDOW_COUNT - WINDOW_SECONDARY_COUNT - i;
                 return PointStatus::Unsatisfied(leap);
             }
         }
@@ -62,14 +85,14 @@ impl Chunker {
 
     fn is_window_qualified(&self, window: &[u8]) -> bool {
         let input = (0..5)
-            .map(|index| window[WINDOW_SIZE - index * WINDOW_MATRIX_SHIFT]) // get elements
-            .map(byte_to_bits)
+            .map(|index| window[WINDOW_SIZE - 1 - index * WINDOW_MATRIX_SHIFT]) // init array
+            .map(byte_to_bits) // transform bytes to bit arrays
             .collect();
 
         let positive_one = self.transform_input(&input, &self.matrix_h);
         let positive_two = self.transform_input(&input, &self.matrix_g);
 
-        return positive_one % 2 == 1 || positive_two % 2 == 1
+        positive_one % 2 == 1 || positive_two % 2 == 1
     }
 
     fn transform_input(&self, input: &Vec<Vec<bool>>, matrix: &Vec<Vec<f64>>) -> usize {
@@ -83,8 +106,9 @@ impl Chunker {
     fn multiply_rows(row_1: &[bool], row_2: &[f64]) -> Vec<f64> {
         row_1
             .iter()
+            .map(|sign| if *sign {1.0} else {-1.0})
             .zip(row_2.iter())
-            .map(|(sign, number)| if *sign { *number } else { -(*number) })
+            .map(|(sign, number)| sign * number)
             .collect()
     }
 }
@@ -96,30 +120,54 @@ fn byte_to_bits(number: u8) -> Vec<bool> {
         .collect()
 }
 
-fn generate_chunks(data: &[u8]) -> Vec<Chunk> {
+pub fn generate_chunks(data: &[u8]) -> Vec<Chunk> {
     let mut chunks = vec![];
     let chunker = Chunker::new();
 
     let mut chunk_start = 0;
     let mut index = MIN_CHUNK_SIZE;
 
+    let mut total = Duration::from_micros(0);
+    let mut times = vec![];
+
     while index < data.len() {
+        let now = Instant::now();
         if index - chunk_start > MAX_CHUNK_SIZE {
             chunks.push(Chunk::new(chunk_start, index - chunk_start));
             chunk_start = index;
             index += MIN_CHUNK_SIZE;
-        }
-
-        match chunker.is_point_satisfied(index, data) {
-            PointStatus::Ok => {
-                chunks.push(Chunk::new(chunk_start, index - chunk_start));
-                chunk_start = index;
-                index += MIN_CHUNK_SIZE;
+            println!("Added chunk: {:?}", chunks.last());
+            if chunks.len() > 1 {
+                println!("Chunks are aligned: {}", chunks[chunks.len() - 2].pos + chunks[chunks.len() - 2].len == chunks.last().unwrap().pos)
             }
-            PointStatus::Unsatisfied(leap) => index += leap,
-        };
+        } else {
+            match chunker.is_point_satisfied(index, data) {
+                PointStatus::Ok => {
+                    chunks.push(Chunk::new(chunk_start, index - chunk_start));
+                    chunk_start = index;
+                    index += MIN_CHUNK_SIZE;
+                    println!("Added chunk: {:?}", chunks.last());
+                    if chunks.len() > 1 {
+                        println!("Chunks are aligned: {}", chunks[chunks.len() - 2].pos + chunks[chunks.len() - 2].len == chunks.last().unwrap().pos)
+                    }
+                }
+                PointStatus::Unsatisfied(leap) => {
+                    index += leap;
+                },
+            };
+        }
+        total = total.add(now.elapsed());
+        times.push(0);
     }
 
+    if index >= data.len() {
+        index = data.len();
+        chunks.push(Chunk::new(chunk_start, index - chunk_start));
+        println!("Added chunk: {:?}", chunks.last());
+        println!("Last chunk is aligned: {}", chunks[chunks.len() - 1].pos + chunks[chunks.len() - 1].len == 660000)
+    }
+
+    println!("{} ms average for an iteration point", total.as_micros() / times.len() as u128);
     chunks
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,25 @@
+use std::env;
+use std::time::Instant;
+use crate::leap_based::generate_chunks;
+
 mod leap_based;
 
 fn main() {
-    println!("Hello, world!");
+    let to_chunk: Vec<u8> = generate_data(660000);
+    println!("Generated data. Calculating chunks...");
+
+    let now = Instant::now();
+
+    let chunks = generate_chunks(&to_chunk);
+
+    println!("{:?}", chunks);
+    println!("{:?}", now.elapsed());
+    let lens: usize = chunks.iter().map(|chunk| chunk.len).sum();
+    println!("Average len: {}", lens / chunks.len());
+}
+
+fn generate_data(size: usize) -> Vec<u8> {
+    (0..size)
+        .map(|_| rand::random::<u8>())
+        .collect()
 }
