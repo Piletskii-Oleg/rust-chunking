2c06dfcd29a1c958b40af31f71edab781eb3c156
Piletskii-Oleg <piletskii.oleg@gmail.com>
2024-03-30 17:49:43 +0300

    remove cli.rs

--- src/bin/filetest.rs
+++ src/bin/filetest.rs
@@ -1,6 +1,3 @@
-mod cli;
-
-use crate::cli::{Algorithm, Cli};
 use chunking::{leap_based, ultra, Chunk};
 use clap::Parser;
 use sha3::{Digest, Sha3_256};
@@ -106,4 +103,37 @@ fn mode(numbers: &[usize]) -> usize {
         .max_by_key(|&(_, count)| count)
         .map(|(val, _)| val)
         .expect("Cannot compute the mode of zero numbers")
+}
+
+#[derive(clap::Parser)]
+#[command(version, about)]
+pub struct Cli {
+    #[command(flatten)]
+    pub input: Input,
+
+    /// Calculate deduplication ratio
+    #[arg(short, long)]
+    pub dedup_ratio: bool,
+
+    /// What algorithm to use on the file
+    #[arg(value_enum)]
+    pub algorithm: Algorithm,
+}
+
+#[derive(clap::Args)]
+#[group(multiple = false)]
+pub struct Input {
+    /// Path to the file to be deduplicated
+    #[arg(short, long, group = "input")]
+    pub path: Option<String>,
+
+    /// Generate data with the given size (in MB) to deduplicate
+    #[arg(short, long, group = "gen", value_name = "size")]
+    pub generate: Option<usize>,
+}
+
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, clap::ValueEnum)]
+pub enum Algorithm {
+    Ultra,
+    Leap,
 }
