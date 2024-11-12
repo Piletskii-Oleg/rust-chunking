817a4cc22619d463989ef409a5331e0e2c4303c6
Piletskii-Oleg <piletskii.oleg@gmail.com>
2024-11-12 13:32:57 +0300

    add SizeParams struct

--- src/lib.rs
+++ src/lib.rs
@@ -14,3 +14,32 @@ impl Chunk {
         Chunk { pos, len }
     }
 }
+
+/// Struct containing size parameters for chunkers:
+/// min, average and max size of chunks.
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
+pub struct SizeParams {
+    pub min_size: usize,
+    pub avg_size: usize,
+    pub max_size: usize,
+}
+
+impl SizeParams {
+    pub fn new(min_size: usize, avg_size: usize, max_size: usize) -> Self {
+        Self {
+            min_size,
+            avg_size,
+            max_size,
+        }
+    }
+}
+
+impl Default for SizeParams {
+    fn default() -> Self {
+        Self {
+            min_size: 2048,
+            avg_size: 4096,
+            max_size: 8192,
+        }
+    }
+}
